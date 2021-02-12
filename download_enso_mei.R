7c6cc6d843a82b01c411ed4d882c73a159742634
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2021-02-11 23:37:34 -0600

    Updated ENSO script.

--- download_enso_mei.R
+++ download_enso_mei.R
@@ -14,7 +14,8 @@ load_mei <- function() {
              skip = 1) %>%
     gather(-year, key = month, value = mei) %>%
     mutate(month = ordered(month, levels = month.abb),
-           date = year + (as.integer(month) - 1) / 12) %>%
+           date = year + (as.integer(month) - 1) / 12,
+           mei = ifelse(mei < -900, NA, mei)) %>%
     arrange(date) %>%
     na.omit()
 
@@ -42,11 +43,28 @@ load_old_mei <- function() {
 
 plot_mei <- function(mei, base_size = 10, limits = c(NA, NA),
                      break_interval = 10) {
-  mei %>% mutate(mei.pos = ifelse(mei >= 0, mei, 0),
-                 mei.neg = ifelse(mei >= 0, 0, mei)) %>%
+  pos_fill = "red"
+  pos_color = pos_fill
+  weak_pos_fill = alpha("red", 0.2)
+  weak_pos_color = alpha("red", 0.3)
+  weak_neg_fill = alpha("blue", 0.2)
+  weak_neg_color = alpha("blue", 0.3)
+  neg_fill = "blue"
+  neg_color = neg_fill
+
+  mei %>% mutate(mei.pos = ifelse(mei > 0.5, mei, 0),
+                 mei.weak_pos = ifelse(mei >= 0 & mei <= 0.5, mei, 0),
+                 mei.weak_neg = ifelse(mei < 0 & mei >= -0.5, mei, 0),
+                 mei.neg = ifelse(mei < -0.5, mei, 0)) %>%
     ggplot(aes(x = date)) +
-    geom_area(aes(y = mei.pos), fill = "red", na.rm = T) +
-    geom_area(aes(y = mei.neg), fill = "blue", na.rm = T) +
+    geom_area(aes(y = mei.neg), color = neg_color, fill = neg_fill,
+              na.rm = TRUE) +
+    geom_area(aes(y = mei.weak_neg), color = weak_neg_color,
+              fill = weak_neg_fill, na.rm = TRUE) +
+    geom_area(aes(y = mei.weak_pos), color = weak_pos_color,
+              fill = weak_pos_fill, na.rm = TRUE) +
+    geom_area(aes(y = mei.pos), color = pos_color, fill = pos_fill,
+              na.rm = TRUE) +
     scale_x_continuous(breaks = seq(1950, 2030, break_interval),
                        limits = limits) +
     labs(x = "Year", y = "Multivariate El Nino Index") +
